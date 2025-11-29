39b93975a43a4c434b314278e20a528f693edaf1
cobosekun <kce.hello@gmail.com>
2025-11-29 09:22:16 +0900

    Fix gitignore to include wp-config-railway.php

---
+++ wp-config-railway.php
@@ -0,0 +1,11 @@
+<?php
+// Railway環境でのURL設定
+if (getenv('RAILWAY_PUBLIC_DOMAIN')) {
+    $site_url = 'https://' . getenv('RAILWAY_PUBLIC_DOMAIN');
+    if (!defined('WP_HOME')) {
+        define('WP_HOME', $site_url);
+    }
+    if (!defined('WP_SITEURL')) {
+        define('WP_SITEURL', $site_url);
+    }
+}
